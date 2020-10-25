50dbbbd07a3b59405448dcead38c99f0bbaf284b
jon2mter <jon.tomter@gmail.com>
2020-10-25 20:33:38 +0100

    trigge api som henter data fra db og viser det på nettside

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -1,8 +1,15 @@
-module.exports = async function (context, req) {
-    context.res = {
-        body: {
-            text: "'klasse-data'"
-
+module.exports = async function (context, req, inndata) { // tar inn databasekoblingen som en parameter
+    var id = context.bindingData.id;
+    if (typeof(inndata[0]) !== "undefined") {   // hvis det finnes data i db med gitt id
+        context.res = {
+            body: {
+                text: JSON.stringify(inndata[0])
+            }
         }
-    };
-};
+    }
+    else context.res = {
+        body: {
+            text: id+' har ingen klasser'
+        }
+    }
+}
