8394121620fa773e3501ac098e7178ce891aa0d0
aamgrim <aamgrim@gmail.com>
2020-10-29 09:16:23 +0100

    Oppdatering gjort i timen

--- klassekart.js
+++ klassekart.js
@@ -1,31 +1,47 @@
 const testClass = ["Åmund", "Erik", "Jon", "Sven", "Magnus", "Mikkel", "Anders", "Kåre", "Pål", "Kjell", "Bonsa"];
 
 function generateClassroom(arr) {
+
+    //   Henter strukturen klassekartet skal genereres på
+    let perTable = parseInt(document.getElementById("perTable").value);
+    let rows = parseInt(document.getElementById("rows").value);
+    let columns = parseInt(document.getElementById("columns").value);
+
     document.getElementById("cr").innerHTML = "";
     let studentsArr = arr.slice(0);
     if (studentsArr.length % 2 === 1) {
         studentsArr.push(".");
     }
     studentsArr = shuffleStudents(studentsArr);
-    studentID = 0;
-    for (let i = 0; i < 5; i++) {
+    let studentID = 0;
+    // Lager flexbox-struktur
+
+    // Først radene
+    for (let i = 0; i < rows; i++) {
         let divRow = document.createElement("div");
         divRow.classList.add("row");
         divRow.id = "row" + i;
         document.getElementById("cr").appendChild(divRow);
-        for (let j = 0; j < 4; j++) {
-            var divPair = document.createElement("div");
-            divPair.classList.add("pair");
-            divPair.id = "r" + i + "c" + j;
-            document.getElementById("row" + i).appendChild(divPair);
-            for (let k = 0; k < 2; k++) {
-                let divStudent = document.createElement("div");
-                divStudent.classList.add("student");
-                divStudent.id = "r" + i + "c" + j + "n" + k;
-                student = (studentsArr[studentID] !== undefined ? studentsArr[studentID] : ".");
-                divStudent.innerHTML = student;
+        // Så pultene
+        for (let j = 0; j < columns; j++) {
+            let divTable = document.createElement("div");
+            divTable.classList.add("table");
+            divTable.id = "r" + i + "c" + j;
+            document.getElementById("row" + i).appendChild(divTable);
+            // Så elevene
+            if (perTable === 1) {
+                document.getElementById("r" + i + "c" + j).innerHTML = picker(studentsArr,studentID);
                 studentID++
-                document.getElementById("r" + i + "c" + j).appendChild(divStudent);
+            }
+            else {
+                for (let k = 0; k < perTable; k++) {
+                    let divStudent = document.createElement("div");
+                    divStudent.classList.add("student");
+                    divStudent.id = "r" + i + "c" + j + "n" + k;
+                    divStudent.innerHTML = picker(studentsArr,studentID);
+                    studentID++
+                    document.getElementById("r" + i + "c" + j).appendChild(divStudent);
+                }
             }
         }
     }
@@ -40,4 +56,9 @@ function shuffleStudents(arr) {
         arr[j] = temp;
     }
     return arr;
+}
+
+function picker(arr,id) {
+    picked = (arr[id] !== undefined ? arr[id] : ".");
+    return picked;
 }
