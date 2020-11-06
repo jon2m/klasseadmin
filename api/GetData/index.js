58b0c5734060921038d781b1b9fc3e85ca6a9081
jon2mter <jon.tomter@gmail.com>
2020-11-06 16:50:23 +0100

    legge til klassedata i select-input

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -1,10 +1,13 @@
-module.exports = async function (context, req, inndata) { // tar inn databasekoblingen som en parameter
+module.exports = async function (context, req, inndata) { // tar inn databasekoblingen som parameter 'inndata'
     var id = context.bindingData.id;
     if (typeof(inndata[0]) !== "undefined") {   // hvis det finnes data i db under gitt id
+        let klasseliste = inndata[0].klasser;
+        let klassekoder = [];
+        for (let i=0; i<klasseliste.length; i++)    // legger til klassekodene i liste
+            klassekoder.push(klasseliste[i].id);
+
         context.res = {
-            body: {
-                text: JSON.stringify(inndata[0])
-            }
+            body: klassekoder        // sender response med klassekoder
         }
     }
     else context.res = {
