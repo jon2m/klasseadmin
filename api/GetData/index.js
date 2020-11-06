6567e0e16bece23d4c1b027b9342e9302bacd804
jon2mter <jon.tomter@gmail.com>
2020-11-06 21:18:29 +0100

    presentere elever etter å ha valgt klasse i index

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -1,18 +1,13 @@
 module.exports = async function (context, req, inndata) { // tar inn databasekoblingen som parameter 'inndata'
     var id = context.bindingData.id;
     if (typeof(inndata[0]) !== "undefined") {   // hvis det finnes data i db under gitt id
-        let klasseliste = inndata[0].klasser;
-        let klassekoder = [];
-        for (let i=0; i<klasseliste.length; i++)    // legger til klassekodene i liste
-            klassekoder.push(klasseliste[i].id);
-
         context.res = {
-            body: klassekoder        // sender response med klassekoder
+            body: inndata[0]        // sender response med klassene til læreren med iden {id}
         }
     }
     else context.res = {
         body: {
-            text: id+' har ingen klasser'
+            text: 'undefined'
         }
     }
 }
