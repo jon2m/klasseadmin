18938165195c0b035ac5dc80cb862de984e6946f
jon2mter <jon.tomter@gmail.com>
2020-11-12 20:01:19 +0100

    backend azure cosmos client

--- api/legg_til_klasse/index.js
+++ api/legg_til_klasse/index.js
@@ -1,8 +1,23 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req, utdata) {  
+    endpoint = "https://db-klasseadmin.documents.azure.com:443/";
+    key = "SMSUiPxTLbJV5IBxhhbImLJ9Yu9p5U1LKf7VuUcnO9osKadnmBYH4LzdKrsXXoSpGgLxcRi3sVInV6aHSHeqVw==";
+    databaseId = 'klasser';
+    containerId = 'laerere';
 
+    const CosmosClient = require("@azure/cosmos").CosmosClient;
+    const client = new CosmosClient({ endpoint, key });
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
     
+    const querySpec = {
+        query: "SELECT * from c"
+    };
+
+    const { resources: items } = await container.items
+        .query(querySpec)
+        .fetchAll();
 
     context.res = {
-        body: 'asdf'
+        body: items
     };
 }
